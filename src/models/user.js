b708c74c9d4c9bd8a308724cef5e1443b7004fee
prajwalmoreCM <prajwal.m@compumatrixtechnologies.com>
2025-05-19 11:48:57 +0530

    first changes

---
+++ src/models/user.js
@@ -0,0 +1,29 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: "customer",
+  },
+  is_active: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+});
+
+export default User;
